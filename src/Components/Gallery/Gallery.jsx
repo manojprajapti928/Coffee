b6a246cc925a671de5fe4687a1dcc75087f3f835
manojprajapti928 <158033172+manojprajapti928@users.noreply.github.com>
2024-07-03 11:21:52 +0530

    Change

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -3,13 +3,16 @@ import './Gallery.css'
 
 export default function Gallery() {
   const coffeeImages = [
-    { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLbU2KU1KS3NuV3X7tWy3zNfLaRuCvQ9x0Xw&s', alt: 'Espresso', title: 'Espresso' },
-    { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2B-AUZ5KEbJfskGz0uPw-JafgYARGxM4_Vw&s', alt: 'Latte', title: 'Latte' },
-    { src: 'https://cdn.pixabay.com/photo/2024/01/19/13/41/cappuccino-8519098_1280.png', alt: 'Cappuccino', title: 'Cappuccino' },
-    { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIhrXJEo9Q-8I6oH7eMcIa03G31Uhmg94rig&s', alt: 'Americano', title: 'Americano' },
+    { src: 'https://png.pngtree.com/png-clipart/20231021/original/pngtree-espresso-coffee-isolated-illustration-ai-generative-png-image_13393912.png', alt: 'Espresso', title: 'Espresso' },
+    { src: 'https://png.pngtree.com/png-clipart/20231002/original/pngtree-top-view-of-a-mug-of-latte-art-coffee-on-timber-png-image_13067750.png', alt: 'Latte', title: 'Latte' },
+    { src: 'https://png.pngtree.com/png-clipart/20230927/original/pngtree-coffee-object-cappuccino-coffee-illustration-3d-png-image_13139619.png', alt: 'Cappuccino', title: 'Cappuccino' },
+    { src: 'https://png.pngtree.com/png-vector/20230413/ourmid/pngtree-americano-coffee-beans-transparent-white-background-png-image_6698453.png', alt: 'Americano', title: 'Americano' },
     { src: 'https://png.pngtree.com/png-clipart/20230927/original/pngtree-mocha-coffee-frappe-in-glass-png-image_13149034.png', alt: 'Mocha', title: 'Mocha' },
     { src: 'https://png.pngtree.com/png-vector/20240206/ourmid/pngtree-watercolor-coffee-macchiato-png-image_11664557.png', alt: 'Macchiato', title: 'Macchiato' },
-  ]
+    { src: 'https://static.vecteezy.com/system/resources/thumbnails/037/280/520/small_2x/ai-generated-black-tea-held-in-a-glass-isolated-on-transparent-background-free-png.png',alt:'Black Tea', title: 'Black tea'},
+    { src: 'https://pngimg.com/d/tea_PNG16895.png',alt:'Herbal Tea', title: 'Herbal Tea'},
+   
+  ];
 
   return (
     <div className="gallery-container">
